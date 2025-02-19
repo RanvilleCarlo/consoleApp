27b4b6a21579f7d52c342e819fb9abd662a7ab9d
ranville <21c46.ranville@sjec.ac.in>
2025-02-19 15:45:15 +0530

    console

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿//encapsulation
+using ConsoleApp1.src.oops.encapsulation;
+
+BadBankAccount badAcoount = new BadBankAccount();
+badAcoount.balance = 1000;
+
+System.Console.WriteLine(badAcoount.balance);
---
+++ ConsoleApp1/src/oops/encapsulation/badbankaccount.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.src.oops.encapsulation
+{
+    public class BadBankAccount
+    {
+        public decimal balance;
+    }
+}
