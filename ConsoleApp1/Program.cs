82edce6bcf1487a2f779bc49fd60b61e0bd373b8
ranville <21c46.ranville@sjec.ac.in>
2025-02-21 20:31:13 +0530

    console

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,20 @@
 ﻿//encapsulation
 
 using ConsoleApp1.src.oops.polymorphism;
+using ConsoleApp1.src.oops.coupling;
 
-List<Vehicle> vehicles = new List<Vehicle>();
-vehicles.Add(new Car{Brand = "Toyota", Model = "Corolla", year = 2019, NumberOfDoors = 4});
-vehicles.Add(new Motorcycle{Brand = "Boeing", Model = "sports", year = 2019});   
-vehicles.Add(new Plane{Brand = "Honda", Model = "Civic", year = 2019, NumberOfDoors = 4});
+// List<Vehicle> vehicles = new List<Vehicle>();
+// vehicles.Add(new Car{Brand = "Toyota", Model = "Corolla", year = 2019, NumberOfDoors = 4});
+// vehicles.Add(new Motorcycle{Brand = "Boeing", Model = "sports", year = 2019});   
+// vehicles.Add(new Plane{Brand = "Honda", Model = "Civic", year = 2019, NumberOfDoors = 4});
  
- //vehicle inspection
+//  //vehicle inspection
 
- foreach (var vehicle in vehicles)
- {
-        vehicle.Start();
-        vehicle.Stop();
- }
+//  foreach (var vehicle in vehicles)
+//  {
+//         vehicle.Start();
+//         vehicle.Stop();
+//  }
+
+var Order = new Order();
  
